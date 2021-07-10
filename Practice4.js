49e612d39bd989ac7fddfcf9c930d3e8b33a1f27
Nishanth Ganesan <nishanthganesan.programming@gmail.com>
2021-07-10 21:48:28 +0530

    Added new file object activity

--- Practice4.js
+++ Practice4.js
@@ -1,20 +1,47 @@
-var myArr = ['a', 'b', 'c', ['d', 'e', 'f']]
+var studentsDetails = {}
+var studentCnt = 0;
 
-for (let index = 0; index < myArr.length; index++) {
+$('document').ready(function(){
+    
+    $('#form-submit').submit(function(){
 
-    if (Array.isArray(myArr[index]) === true) {
 
-        for (let index2 = 0; index2 < myArr[index].length; index2++) {
+        studentCnt++;
 
-            myArr.push(myArr[index][index2])
+        studentsDetails["STUDENT-"+studentCnt]={"Student Name":$('#studentName').val(),"Student Age":+$('#studentAge').val(),"Student School Name":$('#schoolName').val()}
 
-        }
+        // let studentDetailsTable = "<tr><td>"+"student"+studentCnt+"</td><td>"+studentsDetails["student"+studentCnt]["Student Name"]+"</td><td>"+studentsDetails["student"+studentCnt]["Student Age"]+"</td><td>"+studentsDetails["student"+studentCnt]["Student School Name"]+"</td><div><i class='fas fa-trash' id='deleteBtn'></i></div></tr>"
 
-        myArr.splice(index,index-2)
+        var studentDetailsTable = `
 
-    }
+        <tr id='STUDENT-${studentCnt}'>
+            <td>${"STUDENT-"+studentCnt}</td>
+        
+            <td>${studentsDetails["STUDENT-"+studentCnt]["Student Name"]}</td>
 
+            <td>${studentsDetails["STUDENT-"+studentCnt]["Student Age"]}</td>
+            <td>${studentsDetails["STUDENT-"+studentCnt]["Student School Name"]}</td>
+
+            <td><i class='fa fa-trash' onclick='onDelete(${studentCnt})'></i></td>
+
+        </tr>
+
+        
+        `;
+
+
+        console.log(studentsDetails);
+        $('#studentDetailsTable').append(studentDetailsTable)
+        $('#studentName').val("")
+        $('#studentAge').val(0)
+        $('#schoolName').val("")
+
+
+    })
+   
+   
+})
+function onDelete(id){
+    delete studentsDetails["STUDENT-"+id]
+    $('#STUDENT-'+id).remove()
 }
-
-console.log(myArr);
-

a91dd04ecf58bb89fb10f00588c4404247a7b57c
Nishanth Ganesan <nishanthganesan.programming@gmail.com>
2021-07-10 16:37:24 +0530

    added new file - flattening a dimensional array

--- Practice4.js
+++ Practice4.js
@@ -1,1 +1,20 @@
-// Added new file.
+var myArr = ['a', 'b', 'c', ['d', 'e', 'f']]
+
+for (let index = 0; index < myArr.length; index++) {
+
+    if (Array.isArray(myArr[index]) === true) {
+
+        for (let index2 = 0; index2 < myArr[index].length; index2++) {
+
+            myArr.push(myArr[index][index2])
+
+        }
+
+        myArr.splice(index,index-2)
+
+    }
+
+}
+
+console.log(myArr);
+
